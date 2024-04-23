d2484ea80c2f52bfdb1989d69c45104e248cfe22
Vladislav-76 <sillmaril2@gmail.com>
2024-04-23 17:31:34 +0000

    fix: small fix

--- tests/settings.py
+++ tests/settings.py
@@ -8,8 +8,3 @@ REQRES_API_URL_REGISTER = f"{BASE_URL}register/"
 REQRES_API_URL_USERS = f"{BASE_URL}users/"
 
 TEST_SUCCESS_ATTACHMENTS = True
-
-REQRES_HEADERS = {
-    "Content-type": "application/json",
-    "Accept": "text/plain",
-}
